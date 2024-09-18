3aa35b081019a793edbca37aa60f613f36eb1603
ReubenOmollo <reubenochieng23@gmail.com>
2024-09-18 12:03:01 +0300

    first commit

---
+++ numbers.dart
@@ -0,0 +1,26 @@
+// whole numbers (integers)
+// decimals(double)
+
+void main() {
+  int num1 = 43;
+  // double(decimal)
+
+  double num2 = 43.5;
+  var addition= num1+num2;
+  print(addition);
+
+
+ var division = num1/num2;
+  print(division);
+
+
+  var product = num1*num2;
+  print(product);
+
+  var subtraction = num1-num2;
+  print(subtraction);
+
+
+
+
+}
---
+++ variable.dart
@@ -0,0 +1,39 @@
+// variables in dart
+// variables are storage locations
+// void main() {
+//   var name = "marere Ndolo";
+//   var age = 45;
+//   var place = "india";
+//   var marital = "married";
+
+//   print(name);
+//   print(age);
+//   print(place);
+//   print(marital);
+
+// // one sentence that prints all the info
+//   print(
+//       "My name is $name ,I am $age years old and i live in $place.I am $marital");
+// }
+
+// variable in dart
+// variable are storage locations
+void main() {
+  var name = "Reuben Ndolo";
+  var age = 20;
+  var place = "soweto";
+  var hobby = "football";
+  var marital = "single";
+  var contact = "0742095208";
+  var club = "Manchester";
+  var email = "reubenochieng23@gmail.com";
+  print(name);
+  print(age);
+  print(place);
+  print(hobby);
+  print(marital);
+  print(contact);
+// one statement that prints all the info
+  print(
+      "my name is $name,i am $age years old and i live in $place ,i love $hobby,i am $marital, my contact is $contact, my club is $club ,my email is $email ");
+}
